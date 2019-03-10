6a47a660583f15c554b817a1ac19eb30f8ece727
Grayson Martin <grayson@graysonmartin.net>
2019-03-09 19:37:16 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,147 @@
+var ws = require("nodejs-websocket")
+var fs = require("fs")
+var http = require("http"),
+    url = require("url"),
+    path = require("path")
+
+var connections = [] // an array of websocket connections
+var boardData;
+
+/*
+ * Load information from JSON files
+ */
+fs.readFile("data/boardData.json", 'utf8', function(err, data) {
+  if (err) throw err;
+  boardData = JSON.parse(data)
+})
+
+/*
+ * Functions for saving data to JSON files
+ */
+
+function saveBoardData() {
+  fs.writeFile("data/boardData.json", JSON.stringify(boardData), function(err) {
+    if (err) throw err;
+  })
+  sendDataToAllConnections({
+    "job": "updateBoardDisplay",
+    "data": boardData
+  })
+}
+
+/*
+ * Create http server and configure handler function. Then listen on port 80
+ */
+http.createServer(function(req, res) {
+  var uri = url.parse(req.url).pathname
+  switch (uri) {
+    case "/":
+      res.writeHead(200)
+      res.end(fs.readFileSync('./static/index.html'))
+      break;
+    case "/index.js":
+      res.writeHead(200)
+      res.end(fs.readFileSync('./static/index.js'))
+      break;
+    case "/main.css":
+      res.writeHead(200)
+      res.end(fs.readFileSync('./static/main.css'))
+      break;
+    case "/sky.css":
+      res.writeHead(200)
+      res.end(fs.readFileSync('./static/sky.css'))
+      break;
+    default:
+      res.writeHead(308, {"Location": "/"})
+      res.end()
+  }
+}).on("error", function(err) {
+  console.log("HTTP Server threw an error: " + err)
+}).listen(80)
+
+/*
+ * Create websocket server and configure handler function. Then listen on port 8000
+ */
+ws.createServer(function(connection) {
+  connection.on("text", function(str) {
+    try {
+      var data = JSON.parse(str)
+      if (data.job == "subscribe") {
+        connections.push(connection)
+        sendDataToConnection(connection, {
+          "job": "updateBoardDisplay",
+          "data": boardData
+        })
+      } else if (data.job == "createBoardEntry") {
+        createBoardEntry(data)
+      } else if (data.job == "removeBoardEntry") {
+        removeBoardEntry(data)
+      } else if (data.job == "updateBoardEntry") {
+        updateBoardEntry(data)
+      } else if (data.job == "updateBoardFields") {
+        updateBoardFields(data)
+      }
+    } catch {
+      sendDataToConnection(connection, {"error":"invalid request"})
+    }
+  })
+
+  connection.on("close", function() {
+    for (var i = 0; i < connections.length; i++) {
+      if (connection == connections[i]) {
+        connections.splice(i,1)
+        break
+      }
+    }
+  })
+
+  connection.on("error", function(err) {
+    // oop
+  })
+}).listen(8000)
+
+
+/*
+ * Websocket helper functions
+ */
+function sendDataToAllConnections(data) {
+  for (var i = 0; i < connections.length; i++) {
+    sendDataToConnection(connections[i], data)
+  }
+}
+
+function sendDataToConnection(connection, data) {
+  connection.sendText(JSON.stringify(data))
+}
+
+function createBoardEntry(data) {
+  let entry = data.data
+  entry.id = Math.floor(Math.random() * 10000000)
+  boardData.entries.push(entry)
+  saveBoardData()
+}
+
+function removeBoardEntry(data) {
+  for (var i = 0; i < boardData.entries.length; i++) {
+    if (data.data.id == boardData.entries[i].id) {
+      boardData.entries.splice(i, 1)
+      break
+    }
+  }
+  saveBoardData()
+}
+
+function updateBoardEntry(data) {
+  for (var i = 0; i < boardData.entries.length; i++) {
+    if (boardData.entries[i].id == data.data.id) {
+      boardData.entries[i] = data.data
+      break
+    }
+  }
+  saveBoardData()
+}
+
+function updateBoardFields(data) {
+  boardData.fields = data.data.fields
+  saveBoardData()
+}
---
+++ static/index.js
@@ -0,0 +1,188 @@
+var ws = new WebSocket("ws://" + window.location.host + ":8000");
+
+var lastReceivedBoardData = {}
+
+ws.onopen = function() {
+  sendDataToServer({
+    "job": "subscribe"
+  })
+};
+
+ws.onmessage = function(evt) {
+  var received_msg = evt.data;
+  var json_data = JSON.parse(received_msg)
+  if (json_data.job == "updateBoardDisplay") {
+    lastReceivedBoardData = json_data.data
+    setTableData(json_data.data)
+  } else {
+    if (json_data.error) {
+      showError(json_data.error)
+    }
+  }
+};
+
+ws.onclose = function() {
+  // websocket is closed.
+  showError("The connection to the server has failed. Try reloading the page.");
+};
+
+function sendDataToServer(data) {
+  ws.send(JSON.stringify(data));
+}
+
+function setTableData(data) {
+  let headers = "<tr>"
+  for (var i = 0; i < data.fields.length; i++) {
+    headers += `<th style="cursor:pointer;" onclick="showFieldEdit('${data.fields[i].name}')">${data.fields[i].displayName}</th>`
+  }
+  headers += `<th><button class="sky-button-big" onclick="showFieldAdd()">New Column</button></th></tr>`
+
+  let rows = ""
+  for (var i = 0; i < data.entries.length; i++) {
+    rows += `<tr style="cursor:pointer;" onclick="showEdit('${data.entries[i].id}')">`
+    for (var j = 0; j < data.fields.length; j++) {
+      rows += `<td>${data.entries[i][data.fields[j].name] || ""}</td>`
+    }
+    rows += "</tr>"
+  }
+
+  document.getElementById('entryTable').innerHTML = headers + rows
+}
+
+function showError(text) {
+  document.getElementById('showErrors').innerHTML = "<div class=\"sky-alert-message red\">" + text + "</div><br>" + document.getElementById('showErrors').innerHTML
+}
+
+function showFieldAdd() {
+  showPopup(`<h1>Add Column</h1><label for="newFieldName">Name</label><input id="newFieldName"><br><button class="sky-button-big" onclick="addField()">Add</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button>`)
+}
+
+function addField() {
+  hidePopup()
+  lastReceivedBoardData.fields.push({
+    "displayName": document.getElementById('newFieldName').value,
+    "name": document.getElementById('newFieldName').value.toLowerCase().replace(/ /g, "")
+  })
+  sendDataToServer({
+    "job": "updateBoardFields",
+    "data": lastReceivedBoardData
+  })
+}
+
+function showFieldEdit(name) {
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    if (lastReceivedBoardData.fields[i].name == name) {
+      showPopup(`<h1>Edit Column</h1><label for="editFieldName">Name</label><input value="${lastReceivedBoardData.fields[i].displayName}" id="editFieldName"><br><button class="sky-button-big" onclick="editField('${lastReceivedBoardData.fields[i].name}')">Save</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button><button class="sky-button-big right" onclick="promptFieldDelete('${lastReceivedBoardData.fields[i].name}')">Delete</button>`)
+      break
+    }
+  }
+}
+
+function editField(name) {
+  hidePopup()
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    if (lastReceivedBoardData.fields[i].name == name) {
+      lastReceivedBoardData.fields[i].displayName = document.getElementById('editFieldName').value
+      break
+    }
+  }
+  sendDataToServer({
+    "job": "updateBoardFields",
+    "data": lastReceivedBoardData
+  })
+}
+
+function promptFieldDelete(name) {
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    if (lastReceivedBoardData.fields[i].name == name) {
+      showPopup(`<h1>Delete Column</h1><button class="sky-button-big" onclick="deleteField('${name}')">Delete</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button>`)
+      break
+    }
+  }
+}
+
+function deleteField(name) {
+  hidePopup()
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    if (lastReceivedBoardData.fields[i].name == name) {
+      lastReceivedBoardData.fields.splice(i, 1)
+      break
+    }
+  }
+  sendDataToServer({
+    "job": "updateBoardFields",
+    "data": lastReceivedBoardData
+  })
+}
+
+function showAdd() {
+  let html = "<h1>Add Entry</h1>"
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    html += `<label for="newEntryField_${lastReceivedBoardData.fields[i].name}">${lastReceivedBoardData.fields[i].displayName}</label><input type="text" id="newEntryField_${lastReceivedBoardData.fields[i].name}"><br>`
+  }
+  html += `<button class="sky-button-big" onclick="addEntry()">Add</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button>`
+  showPopup(html)
+}
+
+function addEntry() {
+  hidePopup()
+  let data = {}
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    data[lastReceivedBoardData.fields[i].name] = document.getElementById(`newEntryField_${lastReceivedBoardData.fields[i].name}`).value
+  }
+  sendDataToServer({
+    "job": "createBoardEntry",
+    "data": data
+  })
+}
+
+function showEdit(id) {
+  for (var i = 0; i < lastReceivedBoardData.entries.length; i++) {
+    console.log()
+    if (lastReceivedBoardData.entries[i].id == id) {
+      let html = "<h1>Edit Entry</h1>"
+      for (var j = 0; j < lastReceivedBoardData.fields.length; j++) {
+        html += `<label for="editEntryField_${lastReceivedBoardData.fields[j].name}">${lastReceivedBoardData.fields[j].displayName}</label><input type="text" value="${lastReceivedBoardData.entries[i][lastReceivedBoardData.fields[j].name] || ""}" id="editEntryField_${lastReceivedBoardData.fields[j].name}"><br>`
+      }
+      html += `<button class="sky-button-big" onclick="editEntry('${id}')">Save</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button><button class="sky-button-big right" onclick="promptDelete('${id}')">Delete</button>`
+      showPopup(html)
+      break
+    }
+  }
+}
+
+function editEntry(id) {
+  hidePopup()
+  let data = {}
+  for (var i = 0; i < lastReceivedBoardData.fields.length; i++) {
+    data[lastReceivedBoardData.fields[i].name] = document.getElementById(`editEntryField_${lastReceivedBoardData.fields[i].name}`).value
+  }
+  data.id = id
+  sendDataToServer({
+    "job": "updateBoardEntry",
+    "data": data
+  })
+}
+
+function promptDelete(id) {
+  showPopup(`<h1>Delete Entry</h1><button class="sky-button-big" onclick="deleteEntry('${id}')">Delete</button><button class="sky-button-big" onclick="hidePopup()">Cancel</button>`)
+}
+
+function deleteEntry(id) {
+  hidePopup()
+  sendDataToServer({
+    "job": "removeBoardEntry",
+    "data": {
+      "id": id
+    }
+  })
+}
+
+function showPopup(html) {
+  document.getElementById('popup').innerHTML = html
+  document.getElementById('overlay').style = "visibility:visible;"
+}
+
+function hidePopup() {
+  document.getElementById('overlay').style = "visibility:hidden;"
+}
